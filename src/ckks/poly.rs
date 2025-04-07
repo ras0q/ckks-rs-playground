450abd79d1d911efd5c2e44a5f8444ed5065603f
ras0q <ras0q@users.noreply.github.com>
2025-04-07 20:47:51 +0900

    fix div

--- src/ckks/poly.rs
+++ src/ckks/poly.rs
@@ -1,8 +1,13 @@
-use std::ops::{Add, Div, Mul, Neg, Range, Sub};
+use std::{
+    fmt::Debug,
+    ops::{Add, Div, Mul, Neg, Range, Sub},
+};
 
+use num_integer::{Integer, gcd};
 use num_traits::Num;
 
 #[derive(Debug, Clone, Copy)]
+// Polynomial expression on (ℤ/nℤ)[X]/(X^N + 1)
 // P(X) = coeffs[0] + coeffs[1]*X + ... + coeffs[N-1]*X^(N-1)
 pub struct Polynomial<T, const N: usize> {
     pub coeffs: [T; N],
@@ -23,7 +28,7 @@ impl<T, const N: usize> Polynomial<T, N> {
         Self { coeffs, modulo }
     }
 
-    // [x/2, x/2)の範囲に収める
+    // (-x/2, x/2]の範囲に収める
     fn cmod(&self, x: T) -> T
     where
         T: Copy + PartialOrd + Num + From<i64>,
@@ -36,6 +41,16 @@ impl<T, const N: usize> Polynomial<T, N> {
         } else {
             t - modulo
         }
+    }
+
+    fn is_in_range(&self, x: T) -> bool
+    where
+        T: Copy + PartialOrd + Num + From<i64> + Neg<Output = T>,
+    {
+        let modulo = T::from(self.modulo);
+        let half_modulo = modulo / T::from(2);
+
+        x > -half_modulo && x <= half_modulo
     }
 }
 
@@ -98,7 +113,7 @@ where
                 product[i + j] = self.cmod(product[i + j] + self.cmod(*a * *b));
             }
         }
-        // 2N-1次式をN-1次式にする
+        // 2(N-1)次式をN-1次式にする
         // X^N + 1で割ってN-1次式にする
         let mut new_coeffs: [T; N] = [T::zero(); N];
         for i in 0..(N - 1) {
@@ -124,13 +139,115 @@ where
 
 impl<T, const N: usize> Div<T> for Polynomial<T, N>
 where
-    T: Copy + PartialOrd + Num + From<i64>,
+    T: Copy + PartialOrd + Num + From<i64> + Neg<Output = T> + Integer + Debug,
 {
     type Output = Self;
 
     fn div(self, rhs: T) -> Self::Output {
-        let new_coeffs: [T; N] = self.coeffs.map(|c| self.cmod(c / rhs));
+        if rhs == T::one() {
+            return self;
+        }
+        if rhs + T::one() == T::zero() {
+            return -self;
+        }
+
+        if rhs == T::zero() {
+            panic!("Division by zero");
+        }
+        if !self.is_in_range(rhs) {
+            panic!("Division out of range");
+        }
+        if gcd(rhs, T::from(self.modulo)) != T::one() {
+            panic!("Division not coprime");
+        }
+
+        let modulo = T::from(self.modulo);
+        let rhs_inv = {
+            let mut t = T::one();
+            while (t * rhs) % modulo != T::one() {
+                t = t + T::one();
+            }
+            t
+        };
+        println!("rhs_inv: {:?}", rhs_inv);
+
+        let new_coeffs: [T; N] = self.coeffs.map(|c| self.cmod(c * rhs_inv));
 
         Self::new(new_coeffs, self.modulo)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn cmod() {
+        use super::*;
+
+        let poly = Polynomial::<i64, 1>::new([0], 5);
+        assert_eq!(poly.cmod(0), 0);
+        assert_eq!(poly.cmod(1), 1);
+        assert_eq!(poly.cmod(2), -3);
+        assert_eq!(poly.cmod(3), -2);
+        assert_eq!(poly.cmod(4), -1);
+        assert_eq!(poly.cmod(5), 0);
+    }
+
+    #[test]
+    fn neg() {
+        use super::*;
+
+        let poly = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let neg_poly = -poly;
+        assert_eq!(neg_poly.coeffs, [-1, -2, -3, -4]);
+    }
+
+    #[test]
+    fn add() {
+        use super::*;
+
+        let poly1 = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let poly2 = Polynomial::<i64, 4>::new([4, 3, 2, 1], 5);
+        let sum_poly = poly1 + poly2;
+        assert_eq!(sum_poly.coeffs, [0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn sub() {
+        use super::*;
+
+        let poly1 = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let poly2 = Polynomial::<i64, 4>::new([4, 3, 2, 1], 5);
+        let diff_poly = poly1 - poly2;
+        assert_eq!(diff_poly.coeffs, [-3, -1, 1, -2]);
+    }
+
+    #[test]
+    fn mul() {
+        use super::*;
+
+        let poly1 = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let poly2 = Polynomial::<i64, 4>::new([4, 3, 2, 1], 5);
+        let prod_poly = poly1 * poly2;
+        assert_eq!(prod_poly.coeffs, [-1, 0, 1, 0]);
+    }
+
+    #[test]
+    fn mul_scalar() {
+        use super::*;
+
+        let poly = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let scalar = 2;
+        let prod_poly = poly * scalar;
+        assert_eq!(prod_poly.coeffs, [-3, -1, 1, -2]);
+    }
+
+    #[test]
+    fn div_scalar() {
+        use super::*;
+
+        let poly = Polynomial::<i64, 4>::new([1, 2, 3, 4], 5);
+        let scalar = 2;
+        let div_poly = poly / scalar;
+        assert_eq!(div_poly.coeffs, [-2, 1, -1, -3]);
     }
 }
